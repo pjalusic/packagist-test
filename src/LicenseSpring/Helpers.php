0671871b8f08496625e0a5dd44665af55915a4e1
Petar Jalusic <pjalusic@gmail.com>
2019-09-11 11:36:04 +0200

    #3

---
+++ src/LicenseSpring/Helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LicenseSpring;
+
+require_once '../../vendor/autoload.php';
+
+class Helpers {
+  
+  public function __construct() {
+  }
+
+}
---
+++ src/LicenseSpring/Webhook.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LicenseSpring;
+
+require_once '../../vendor/autoload.php';
+require_once 'Helpers.php';
+
+class Webhook {
+  
+  public function __construct() {
+    $helper = new Helpers();
+  }
+
+}
